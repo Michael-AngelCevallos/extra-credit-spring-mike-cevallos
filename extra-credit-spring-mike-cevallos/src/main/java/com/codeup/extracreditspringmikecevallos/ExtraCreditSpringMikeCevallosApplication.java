308b42f3332cef0d732f674bb5d2bdbdbdb64db1
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-03-30 15:26:56 -0500

    all starters are fixed

---
+++ extra-credit-spring-mike-cevallos/src/main/java/com/codeup/extracreditspringmikecevallos/ExtraCreditSpringMikeCevallosApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.extracreditspringmikecevallos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraCreditSpringMikeCevallosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraCreditSpringMikeCevallosApplication.class, args);
+	}
+
+}
---
+++ extra-credit-spring-mike-cevallos/src/test/java/com/codeup/extracreditspringmikecevallos/ExtraCreditSpringMikeCevallosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.extracreditspringmikecevallos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraCreditSpringMikeCevallosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
