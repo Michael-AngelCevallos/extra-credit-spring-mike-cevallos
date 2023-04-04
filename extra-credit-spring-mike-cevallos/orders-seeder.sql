679978117c82ac77f67472e823d0d3adba78efaa
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-04-04 13:52:08 -0500

    added  create a post button

---
+++ extra-credit-spring-mike-cevallos/orders-seeder.sql
@@ -0,0 +1,7 @@
+use extra_cred_records_db;
+
+insert into orders(email, total_price) VALUES
+   ( 'john@yahoo.com', 873475),
+   ( 'mike@github.com', 8475),
+   ( 'Jovan@gmail.com', 9839120),
+   ( 'BigHulk.com', 32876);
